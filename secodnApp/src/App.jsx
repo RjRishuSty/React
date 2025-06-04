7d0863f68f7be9961486133339a8fdeb7a1987ef
RjRishuSty <rishurajk59@gmail.com>
2025-06-05 03:49:55 +0530

    done

--- secodnApp/src/App.jsx
+++ secodnApp/src/App.jsx
@@ -5,9 +5,15 @@ import Cart from "./components/Cart";
 
 const App = () => {
   const [cartItems, setCartItem] = useState([]);
+  const [cartStatus, setCartStatus] = useState(false);
 
   const handleAddToCart = useCallback((item) => {
     setCartItem((prev) => [...prev, item]);
+    setCartStatus(true)
+  }, []);
+
+  const handleCloseCart = useCallback(() => {
+    setCartStatus((prev) => !prev);
   }, []);
 
   useEffect(() => {
@@ -16,14 +22,15 @@ const App = () => {
 
   return (
     <Grid container spacing={2}>
-      <Grid size={{xs:12,sm:12,md:12}} sx={{  order:{xs:2,sm:2,md:1} }}>
+      <Grid
+        size={{ xs: 12, sm: 12, md: 12 }}
+        sx={{ order: { xs: 2, sm: 2, md: 1 } }}
+      >
         <ProductCard handleAddToCart={handleAddToCart} />
       </Grid>
 
-      {cartItems.length > 0 && (
-        <Grid size={{xs:12,sm:12,md:12}} sx={{order:{xs:1,sm:1,md:2}}}>
-          <Cart cartItems={cartItems} />
-        </Grid>
+      {cartItems.length > 0 && cartStatus && (
+        <Cart cartItems={cartItems} handleCloseCart={handleCloseCart} />
       )}
     </Grid>
   );
--- secodnApp/src/components/Cart.jsx
+++ secodnApp/src/components/Cart.jsx
@@ -1,32 +1,60 @@
-import { Box, Stack, Typography } from "@mui/material";
+import { Box, IconButton, Stack, Typography } from "@mui/material";
 import React from "react";
+import CloseIcon from "@mui/icons-material/Close";
 
-const Cart = ({ cartItems }) => {
+const Cart = ({ cartItems ,handleCloseCart}) => {
   if (!cartItems) return;
   console.log(cartItems, "cartItems");
   const handleTotal = () => {
     return cartItems.reduce((acc, item) => acc + item.price, 0);
   };
   return (
-    <Stack sx={{ width: "100%", py:10 }}>
-      <Typography variant="h5" sx={{ fontWeight: 700 }}>
-        Cart
-      </Typography>
+    <Stack
+      sx={{
+        width: "auto",
+        height: "100vh",
+        position: "absolute",
+        top: 0,
+        right: 0,
+        padding: "0px 15px",
+        backgroundColor:'rgb(154, 223, 154)'
+      }}
+    >
+      <Box
+        sx={{
+            mt:5,
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center",
+        }}
+      >
+        <Typography variant="h5" sx={{ fontWeight: 700 }}>
+          Cart
+        </Typography>
+        <IconButton onClick={handleCloseCart}>
+          <CloseIcon fontSize="large" sx={{ color: "white" }} />
+        </IconButton>
+      </Box>
       <Box
         sx={{
           width: "auto",
-          height: "40vh",
+          height: "85vh",
           overflowY: "auto",
         }}
       >
-        {cartItems?.map((item) => (
-          <Box sx={{ p: 2 }}>
+        {cartItems?.map((item, index) => (
+          <Box key={index} sx={{ p: 2 }}>
             <Typography>Product Name:- {item.name}</Typography>
             <Typography>Product Price:- ₹{item.price}</Typography>
           </Box>
         ))}
       </Box>
-      <Typography variant="h5" sx={{backgroundColor:'green',p:2, color:'white'}}>Total Price:- ₹{handleTotal()}</Typography>
+      <Typography
+        variant="h5"
+        sx={{ backgroundColor: "green", p: 2, color: "white" }}
+      >
+        Total Price:- ₹{handleTotal()}
+      </Typography>
     </Stack>
   );
 };
